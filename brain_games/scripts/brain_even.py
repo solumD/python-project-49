14e8d3eb4058bf9542af5d67f021543676c2435f
Dmitriy <dimkkononov@gmail.com>
2023-03-14 21:26:53 +0300

    add new game - even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from ..cli import welcome_user
+from random import randint
+
+def main():
+    print('Welcome to the Brain Games!')
+    n = welcome_user()
+    start_round(n)
+    goodbye(n)
+
+
+def start_round(arg):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    j = 0
+    while j < 3:
+        ch = randint(0, 100)
+        diff = ch % 2
+        print(f'Question: {ch}')
+        answer = input('Your answer: ')
+        if diff == 1 and answer == 'no':
+            j += 1
+            print('Correct!')
+        if diff == 0 and answer == 'yes':
+            j += 1
+            print('Correct!')
+        if diff == 1 and answer != 'no':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+            print(f"Let's try again, {arg}!")
+            j = 0
+        if diff == 0 and answer != 'yes':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+            print(f"Let's try again, {arg}!")
+            j = 0
+
+
+def goodbye(arg):
+    return print(f'Congratulations, {arg}!')
+
+
+if __name__ == '__main__':
+    main()
